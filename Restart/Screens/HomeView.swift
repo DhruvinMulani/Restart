e443bd4289b47011a57e1e76068a5ea6547cebaa
mulanidhruvin <mulanidhruvin@gmail.com>
2024-02-05 15:56:12 -0500

    restart app build

---
+++ Restart/Screens/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  Restart
+//
+//  Created by Dhruvin Mulani on 2024-02-05.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
---
+++ Restart/Screens/OnbordingView.swift
@@ -0,0 +1,18 @@
+//
+//  OnbordingView.swift
+//  Restart
+//
+//  Created by Dhruvin Mulani on 2024-02-05.
+//
+
+import SwiftUI
+
+struct OnbordingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnbordingView()
+}
