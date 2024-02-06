f3a75ac1764d7f95418fef68ec5091bcc72837d0
DhruvinMulani <mulanidhruvin@gmail.com>
2024-02-05 20:27:09 -0500

    Created onboardView and Home View Screen both are in progress

---
+++ Restart/Views/CircleGroupView.swift
@@ -0,0 +1,18 @@
+//
+//  CircleGroupView.swift
+//  Restart
+//
+//  Created by Dhruvin Mulani on 2024-02-05.
+//
+
+import SwiftUI
+
+struct CircleGroupView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleGroupView()
+}
