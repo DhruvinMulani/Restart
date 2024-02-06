420141fa13e3d0db84b42813f0ec103f79d8851d
DhruvinMulani <mulanidhruvin@gmail.com>
2024-02-05 20:29:52 -0500

    created homeView and onBordding screen, with basic image and button UI

--- Restart/Screens/HomeView.swift
+++ Restart/Screens/HomeView.swift
@@ -13,17 +13,41 @@ struct HomeView: View {
     @AppStorage("onbording") var isOnbordingViewActive: Bool = false
 
     var body: some View {
-        VStack(spacing: 20){
-        
-            Text("Home")
-                .font(.largeTitle)
-            
-            Button(action: {
-                isOnbordingViewActive=true
-            }) {
-                Text("Restart")
+        VStack(spacing:20){
+           
+            //MARK:- Header
+            Spacer()
+            ZStack {
+                CircleGroupView(ShapeColor: .gray, ShapeOpacity: 0.1)
+
+                Image("character-2")
+                    .resizable()
+                    .scaledToFit()
+                .padding()
             }
-        }
+            //MARK:- Center
+            Text("The time that leads to mastery is dependent on the intensity of our foucs.")
+                .font(.title3)
+                .fontWeight(.light)
+                .foregroundColor(.secondary)
+                .multilineTextAlignment(.center)
+                .padding()
+            //MARK:- Footer
+            Spacer()
+            Button(action: {
+                isOnbordingViewActive = true
+            }){
+                Image(systemName: "arrow.triangle.2.circlepath.circle.fill")
+                    .imageScale(.large)
+                Text("Restart")
+                    .font(.system(.title3, design: .rounded))
+                    .fontWeight(.bold)
+            }//: Button
+            .buttonStyle(.borderedProminent)
+            .buttonBorderShape(.capsule)
+            .controlSize(.large)
+            
+        }//:Vstack
     }
 }
 
--- Restart/Screens/OnbordingView.swift
+++ Restart/Screens/OnbordingView.swift
@@ -12,16 +12,98 @@ struct OnbordingView: View {
     
     @AppStorage("onbording") var isOnbordingViewActive: Bool = true
     var body: some View {
-        VStack {
-            Text("Onbording")
-                .font(.largeTitle)
-            
-            Button(action:{
-isOnbordingViewActive = false
-            }) {
-                Text("Start")
-            }
-        }
+        ZStack {
+            Color("ColorBlue")
+                .ignoresSafeArea (.all, edges: .all)
+            VStack(spacing:20){
+                //MARK:- Header
+                Spacer()
+                
+                VStack(spacing:0){
+                    Text("Share.")
+                        .font(.system(size: 60))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                    
+                    Text("""
+                    It's not hopw much we give but
+                    how much love we put into giving.
+                    """)
+                        .font(.title3)
+                        .fontWeight(.light)
+                        .multilineTextAlignment(.center)
+                        .foregroundColor(.white)
+                        .padding(.horizontal,10)
+                }
+                
+                //MARK:- Center
+                
+                ZStack{
+                    CircleGroupView(ShapeColor: .white, ShapeOpacity: 0.20)
+                   //: ZSTACK
+                    
+                    Image("character-1")
+                        .resizable()
+                        .scaledToFit()
+                                    }//: Center
+                
+                Spacer()
+                //MARK:- Footer
+                
+                ZStack{
+                    //1. Background (Static)
+                    
+                    Capsule()
+                        .fill(Color.white.opacity(0.2))  
+                    Capsule()
+                        .fill(Color.white.opacity(0.2))
+                        .padding(8)
+                    //2. call-to-action (Static)
+                    
+                    Text("Get Started")
+                        .font(.system(.title3, design: .rounded))
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .offset(x:20)
+                    //3. capsule (dynamic width)
+                    
+                    HStack{
+                        Capsule()
+                            .fill(Color("ColorRed"))
+                            .frame(width: 80)
+                        
+                        Spacer()
+                    }
+                    //4. circle (draggable)
+                    
+                    HStack {
+                        ZStack{
+                            Circle()
+                                .fill(Color("ColorRed")) 
+                            
+                            Circle()
+                                .fill(.black.opacity(0.15))
+                                .padding(8)
+                            Image(systemName: "chevron.right.2")
+                                .font(.system(size:24, weight: .bold))
+                            
+                        }
+                        .foregroundColor(.white)
+                    .frame(width: 80,height: 80,alignment: .center)
+                    .onTapGesture {
+                        isOnbordingViewActive=false
+                    }
+                        
+                        Spacer()
+                    }
+                    
+                    
+                    
+                }//:Footer
+                .frame(height: 80, alignment: .center)
+                .padding()
+            }//:Vstack
+        }//:Zstack
     }
 }
 
